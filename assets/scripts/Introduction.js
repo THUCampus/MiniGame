22d4b85a1794552d010907ca44998aa8cabcab51
Koala.H.F <fengh16@163.com>
2018-07-29 17:04:06 +0800

    update v0.2.0
    基本完成

--- assets/scripts/Introduction.js
+++ assets/scripts/Introduction.js
@@ -32,7 +32,6 @@ cc.Class({
             index += 1;
             self.content.opacity = 255 * index / self.showTimes;
             setTimeout(()=>{self.showContent(self, index);}, self.singleTime * 1000);
-            console.log(index);
         }
         else if (index >= self.showTimes && self.content) {
             self.hideContent(self, 0);
@@ -66,8 +65,9 @@ cc.Class({
     onLoad() {
         this.data = [
             '曾经，有一位勇士',
+            '名叫圆圆',
             '和一位公主',
-            '成为眷侣',
+            '沉入爱河',
             '他们没日没夜',
             '聊着微信',
             '互发动图',
@@ -76,11 +76,26 @@ cc.Class({
             '自从他沉迷表情包',
             '就再未正常聊天',
             '每日斗图为乐',
-            '日渐消瘦',
-            '......',
-            '剩下的你来写吧',
-            '我先睡了',
-            '早安',
+            '日渐变圆',
+            '公主劝说无效',
+            '只得以泪洗面',
+            '有人带来了\n遥远东方的古老魔法',
+            '公主沉思已久',
+            '最终将圆圆封印',
+            '成了一个真正的\n圆圆的球',
+            '圆圆痛定思痛',
+            '习得\n放冰术与碎冰术',
+            '踏上了寻回公主真心的旅途',
+            '但这一路上并不平坦',
+            '他曾经发过的表情包\n仍然在诱惑着他',
+            '一不留神他就会再次沉迷',
+            '永远失去他的公主',
+            '但还有好心人',
+            '在他前行的道路上准备好了',
+            '各种道具',
+            '助他一臂之力',
+            '准备好了吗',
+            '游戏即将开始',
         ];
         this.button.active = false;
         this.showSentence(this);
